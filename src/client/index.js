bba30bd0dc6c03f9fa3f526900b1f15f80ced779
Erin Dachtler <download333@gmail.com>
2017-01-16 12:47:55 -0800

    Self-hosting doesn't need https redirect

--- src/client/index.js
+++ src/client/index.js
@@ -4,18 +4,8 @@ import Podcasts from './components/podcasts';
 import Player from './components/player';
 
 
-if(location.protocol === 'http:' && location.hostname !== 'localhost') {
-  location.replace('https://' + location.host + location.pathname);
-}
-
 if(navigator.serviceWorker) {
   navigator.serviceWorker.register('/sw.js');
-}
-
-const place = localStorage.getItem('place');
-if(place) {
-  localStorage.setItem('mbmbam_place', place);
-  localStorage.removeItem('place');
 }
 
 render(
